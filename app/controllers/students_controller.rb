48a104a91907d31d3999a3caf20d1ad952276742
Patrick Kim <pkim050@gmail.com>
2019-04-23 13:07:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,6 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-
+    binding.pry
   end
 end
